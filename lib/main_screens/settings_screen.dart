6c6daaa4b1b52dc95ea27f8448f4916cf26b7c5e
AM <ammar260813@gmail.com>
2022-02-22 01:29:57 +0100

    GDPR Compliance (User delete own account) typo fix

--- lib/main_screens/settings_screen.dart
+++ lib/main_screens/settings_screen.dart
@@ -313,7 +313,7 @@ class _State extends State<SettingsScreen> {
                               return Form(
                                 autovalidateMode: AutovalidateMode.onUserInteraction,
                                 key: _authUserkey,
-                                child: AlertDialog(title: const Text("Autentificer konto"), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)), content: const Text("Du bedes indtaste din gamle E-mail og adgangskode for at komme videre."), actions: [
+                                child: AlertDialog(title: const Text("Autentificer konto"), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)), content: const Text("Du bedes indtaste din nuværende E-mail og adgangskode for at komme videre."), actions: [
                                   TextFormField(validator: validateEmail, controller: emailController, decoration: const InputDecoration(icon: Icon(Icons.email), hintText: "E-mail", hintMaxLines: 10,),),
                                   TextFormField(validator: validatePassword, controller: passwordController, obscureText: true, decoration: const InputDecoration(icon: Icon(Icons.password), hintText: "Adgangskode", hintMaxLines: 10,),),
                                   TextButton(onPressed: () async {
