b9f1a7aca4a5638f6960cf9fd143e2b8e4a8608d
Heidelbergg <amercol2608A>
2022-02-28 03:28:06 +0100

    working iOS version (Do not use xcode to install dependencies for firebase etc! Pubspeck.yaml is sufficient).
    Steps to reproduce iOS installation:
    1. Follow iOS project setup on firebase
    2. DO NOT ADD PACKAGE DEPENCIES IN XCODE
    3. In terminal (cd into project) -> make sure cocoapods and ffi is installed -> cd into ios -> pod init -> edit pod to contain iOS v14> -> pod install -> run project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:odinvikar/admin/admin_dashboard.dart';
 import 'package:odinvikar/intro_screen.dart';
