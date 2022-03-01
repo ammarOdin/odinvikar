375a900e1b8b49c26bfde8beae2aafe0deb6631d
Heidelbergg <amercol2608A>
2022-03-01 01:37:05 +0100

    Alert dialog change

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 bool screen = true;
-FirebaseMessaging messaging = FirebaseMessaging.instance;
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
   await messaging.requestPermission(
     alert: true,
     announcement: false,
