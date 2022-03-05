b7a43b3f614a53f2b3c4b953283eb04454b5acb7
Heidelbergg <amercol2608A>
2022-03-05 03:31:38 +0100

    Rounded alertbox feedback

--- lib/admin/admin_settings_screen.dart
+++ lib/admin/admin_settings_screen.dart
@@ -320,6 +320,7 @@ class _State extends State<AdminSettingsScreen> {
                   return Form(
                     key: _feedbackKey,
                     child: AlertDialog(title: const Text("Feedback"),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
                       content: const Text("Er der fejl eller mangler? Eller har du forslag til forbedringer? Brug nedenstående felt."),
                       actions: [
                         TextFormField(validator: validateFeedbackField, controller: feedbackController, decoration: const InputDecoration(icon: Icon(Icons.feedback), hintText: "Besked"),),
