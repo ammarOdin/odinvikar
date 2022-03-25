b1a9470150cdd7fd156eeb86f4982db106fe0e8e
AM <ammar260813@gmail.com>
2022-03-25 14:12:43 +0100

    Calendar updates when adding new shift

--- lib/main_screens/own_days.dart
+++ lib/main_screens/own_days.dart
@@ -158,7 +158,8 @@ class OwnDays extends State<OwnDaysScreen> {
                 height: 50,
                 margin: const EdgeInsets.only(bottom: 5, left: 5, right: 5, top: 10),
                 child: ElevatedButton.icon(onPressed: () async {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => OwnDaysDatepicker())).then((value) {setState(() {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => OwnDaysDatepicker())).then((value) {
+                    setState(() {
                     getFirestoreShift();
                   });});
                   }, icon: const Icon(Icons.add_circle), label: const Align(alignment: Alignment.centerLeft, child: Text("Tilføj Dag")), style: ButtonStyle(shape: MaterialStateProperty.all(
