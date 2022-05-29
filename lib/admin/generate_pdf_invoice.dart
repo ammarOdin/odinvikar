4d527cba73d3328fcf0906e49cd6b5f43cec87ce
AM <ammar260813@gmail.com>
2022-05-29 04:05:56 +0200

    invoice work

--- lib/admin/generate_pdf_invoice.dart
+++ lib/admin/generate_pdf_invoice.dart
@@ -1,8 +1,8 @@
 import 'dart:html';
+import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
-import 'package:flutter/material.dart';
 
 class PdfApi {
 
@@ -39,5 +39,15 @@ class PdfApi {
     final dir = await getApplicationDocumentsDirectory();
     final file = File('${dir.path}/$name');
 
+    await file.writeAsBytes(bytes);
+
+    return file;
+
+  }
+
+  static Future openFile(File file) async {
+    final url = file.relativePath;
+
+    await OpenFile.open(url);
   }
 }
